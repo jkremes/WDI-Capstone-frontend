e42377f28a0c3e77e88cc54552f85895c76fd3af
jkremes <only1truth333@gmail.com>
2018-12-07 02:40:31 -0500

    deploy try

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     baseUrl: process.env.NODE_ENV === 'production'
-      ? '/WDI-Capstone-frontend'
+      ? './WDI-Capstone-frontend'
       : '/',
       configureWebpack: {
         devtool: 'source-map'
