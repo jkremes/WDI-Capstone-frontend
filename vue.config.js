c4ccffba7dbb620401856bb057008436e73c515a
jkremes <only1truth333@gmail.com>
2018-12-07 03:43:52 -0500

    another config change to test proper deployment

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,6 @@ module.exports = {
     baseUrl: process.env.NODE_ENV === 'production'
       ? '/WDI-Capstone-frontend/'
       : '/',
-      configureWebpack: {
-        devtool: 'source-map'
-      },
       devServer: {
         proxy: 'https://mysterious-mesa-43439.herokuapp.com/'
       }
