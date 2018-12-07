5fbbb96fa7885547244019903891d0c093e22dcf
jkremes <only1truth333@gmail.com>
2018-12-07 04:47:09 -0500

    Revert "another config change to test proper deployment"
    This reverts commit d6135f82e6b52bbff6a970cab285123aae714bc3.

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,27 +1,16 @@
 import axios from 'axios'
 // import config from '../config'
-let apiUrl
-const apiUrls = {
-  production: 'https://mysterious-mesa-43439.herokuapp.com/',
-  development: 'http://localhost:4741'
-}
-
-if (window.location.hostname === 'localhost') {
-  apiUrl = apiUrls.development
-} else {
-  apiUrl = apiUrls.production
-}
 
 export default() => {
   return axios.create({
-    baseURL: apiUrl
+    // baseURL: config.apiUrl
     // baseURL: `http://localhost:4741`
     // baseUrl: process.env.NODE_ENV === 'production'
     // ? '/production-sub-path/'
     // : '/'
-    // baseUrl: process.env.NODE_ENV === 'production'
-    //   ? '/WDI-Capstone-frontend/'
-    //   : '/'
+    baseUrl: process.env.NODE_ENV === 'production'
+      ? '/WDI-Capstone-frontend/'
+      : '/'
   })
 }
 
--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,3 @@
-// let apiUrl
-// const apiUrls = {
-//   production: 'https://mysterious-mesa-43439.herokuapp.com/',
-//   development: 'http://localhost:4741'
-// }
-
-// if (process.env.NODE_ENV === 'localhost') {
-//   apiUrl = apiUrls.development
-// } else {
-//   apiUrl = apiUrls.production
-// }
-
 module.exports = {
     baseUrl: process.env.NODE_ENV === 'production'
       ? '/WDI-Capstone-frontend/'
@@ -18,7 +6,6 @@ module.exports = {
         './src',
       devServer: {
         proxy: 'https://mysterious-mesa-43439.herokuapp.com/'
-        // proxy: 'http://localhost:4741'
       }
   }
   
